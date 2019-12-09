85a357e3ceb9f4c4ac7a1a75aa525c3f85527e8f
Junior Garcia <osvalgarcia4000@gmail.com>
2019-12-09 15:53:53 -0300

    Correccion de host permitidos en production

--- Web_GrupTech/wsgi.py
+++ Web_GrupTech/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_GrupTech.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_GrupTech.settings.production')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_GrupTech.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_GrupTech.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
