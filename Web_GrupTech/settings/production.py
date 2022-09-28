9119c43947c7bbaaab2ee243af9bb317806ba7fd
rossanapurizaca <rpurizaca@grupo-tech.com>
2022-09-28 12:04:18 -0300

    Comento use_tz

--- Web_GrupTech/settings/production.py
+++ Web_GrupTech/settings/production.py
@@ -9,21 +9,21 @@ ALLOWED_HOSTS = ['127.0.0.1', 'grupotech.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if not IS_HEROKU:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'dmgdh1ph6jhif',
-            'USER': 'ktwywpzwdeoftz',
-            'PASSWORD': '8cc7163b029be691f437ce33411534d562fd19ae9afa0e378e2923282844ff54',
-            'HOST': 'ec2-54-91-223-99.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dmgdh1ph6jhif',
+        'USER': 'ktwywpzwdeoftz',
+        'PASSWORD': '8cc7163b029be691f437ce33411534d562fd19ae9afa0e378e2923282844ff54',
+        'HOST': 'ec2-54-91-223-99.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
+}
