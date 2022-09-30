2c09fd522192a86f44957d03f65b79ca186773cc
rossanapurizaca <rpurizaca@grupo-tech.com>
2022-09-30 16:16:37 -0300

    Agrego CSRF_TRUSTED_ORIGINS

--- Web_GrupTech/settings/production.py
+++ Web_GrupTech/settings/production.py
@@ -2,12 +2,12 @@ from .base import *
 from django.http import HttpResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
 
-IS_HEROKU = "DYNO" in os.environ
+#IS_HEROKU = "DYNO" in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:
-    DEBUG = True
-else:
-    X_FRAME_OPTIONS = 'web.grupo-tech.com'
+#DEBUG = True
+X_FRAME_OPTIONS = 'web.grupo-tech.com'
+DEBUG = False
+CSRF_TRUSTED_ORIGINS = ['https://web.grupo-tech.com']
 
 ALLOWED_HOSTS = ['127.0.0.1', 'grupotech.herokuapp.com', 'web.grupo-tech.com']
 
